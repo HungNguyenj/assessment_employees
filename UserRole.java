ef3926e9a5e08e0034560bb1df289f7edd1b13cf
Phuong-Bk <121529266+Phuong-Bk@users.noreply.github.com>
2025-07-10 09:34:10 +0700

    Delete UserRole.java

--- UserRole.java
+++
@@ -1,7 +0,0 @@
-package com.example.assessment_employees.entity;
-
-public enum UserRole {
-    EMPLOYEE,    // Nhân viên bình thường
-    MANAGER,    // Quản lý nhân sự phòng ban
-    SUPERVISOR      // Admin/Super Admin
-}
