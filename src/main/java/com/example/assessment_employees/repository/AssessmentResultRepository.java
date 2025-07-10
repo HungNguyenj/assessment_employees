f93c825c4e946bcc8361b1ee10a53631432eb5d0
Phuong-Bk <121529266+Phuong-Bk@users.noreply.github.com>
2025-07-10 09:22:38 +0700

    Add files via upload

--- src/main/java/com/example/assessment_employees/repository/AssessmentResultRepository.java
+++ src/main/java/com/example/assessment_employees/repository/AssessmentResultRepository.java
@@ -1,6 +1,7 @@
 package com.example.assessment_employees.repository;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.data.domain.Limit;
 import org.springframework.data.domain.Sort;
@@ -15,5 +16,9 @@ import com.example.assessment_employees.entity.AssessmentResult;
 @Repository
 public interface AssessmentResultRepository extends JpaRepository<AssessmentResult, Integer> {
     List<AssessmentResult> findByAssessedUser_UserId(Integer assessedUserUserId);
+    // Trong AssessmentResultRepository
+    Optional<AssessmentResult> findById(Integer id);
+    List<AssessmentResult> findByAssessedUser_UserIdAndTemplate_TemplateId(Integer userId, Integer templateId);
+
 }
 
