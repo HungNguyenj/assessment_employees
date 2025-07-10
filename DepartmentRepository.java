2ca2d663425313eed2f89fd699ef58de9d15919c
Phuong-Bk <121529266+Phuong-Bk@users.noreply.github.com>
2025-07-10 09:33:40 +0700

    Delete DepartmentRepository.java

--- DepartmentRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.assessment_employees.repository;
-
-import com.example.assessment_employees.entity.Department;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface DepartmentRepository extends JpaRepository<Department, Integer> {
-}
